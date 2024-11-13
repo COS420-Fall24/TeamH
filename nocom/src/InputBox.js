694ee293b57865c92b61505cde45300e4202aaa4
antAmaine <157142587+antAmaine@users.noreply.github.com>
2024-11-13 03:03:31 -0500

    Input boxes

--- nocom/src/InputBox.js
+++ nocom/src/InputBox.js
@@ -7,15 +7,12 @@ function InputBox() {
     if (e.key === 'Tab') {
       e.preventDefault();
       const cursorPosition = e.target.selectionStart;
-      const newValue = value.substring(0, cursorPosition) + '\t' + value.substring(cursorPosition);
+      const newValue = `${value.substring(0, cursorPosition)}\t${value.substring(cursorPosition)}`;
       setValue(newValue);
-      e.target.selectionStart = e.target.selectionEnd = cursorPosition + 1;
     }
   };
   return (
-    <div>
     <textarea value={value} onChange={(e) => setValue(e.target.value)} onKeyDown={handleKeyDown} />
-    </div>
   );
 }
 export default InputBox;

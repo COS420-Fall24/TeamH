1ee9bbe5aaa8ac8d53457d4241ef7d4b1d3f6f22
antAmaine <157142587+antAmaine@users.noreply.github.com>
2024-11-13 03:03:31 -0500

    Input boxes

--- nocom/src/App.js
+++ nocom/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import InputBox from './InputBox';
-import SvgMockup from './SvgMockup'; // Ensure this path matches the location of SvgMockup.js
 import LoginWidget from './LoginWidget'; // Your existing login component
 
 const App = () => {
   return (
     <div>
-      <SvgMockup /> {/* Displays the Screen.svg */}
       <LoginWidget /> {/* Your login form or other content */}
+      <div className='inputbox inp'>
+        <InputBox className='inp'/>
+      </div>
+      <div className='inputbox con'>
+        <InputBox className='con'/>
+      </div>
     </div>
   );
 };

ceed33e72403ff33f6fd70717ecc118fa0ee0adb
Obed Appiah <98442128+obedappiah@users.noreply.github.com>
2024-11-22 21:31:47 -0500

    added Account to App

--- nocom/src/App.js
+++ nocom/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Input from './Input';
 import LoginWidget from './LoginWidget'; // Your existing login component
-
+import Account from './Account';
 const App = () => {
   return (
     <div>

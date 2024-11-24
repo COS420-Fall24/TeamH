67cd051347553347aee72baa2da1fc3c382794a9
antAmaine <157142587+antAmaine@users.noreply.github.com>
2024-11-24 15:07:00 -0500

    Cleaned up CSS and Added tab to Exp
    I cleaned up the CSS in the html declerations
    
    I added the ability to tab in feedback.js
    
    also simplified the tab function

--- nocom/src/components/Feedback/Feedback.js
+++ nocom/src/components/Feedback/Feedback.js
@@ -2,15 +2,29 @@ import React from 'react';
 import './Feedback.css';
 
 function Feedback() {
+  const handleKeyDown = (e) => {
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      let value = e.target.value
+      const cursorPosition = e.target.selectionStart;
+      let i = cursorPosition
+      while(value[i-1] != '\n' && i-1>=0){
+        i--
+      }
+      const newValue = `${value.substring(0, i)}\t${value.substring(i)}`;
+      e.target.value= newValue
+      setTimeout(function(){ e.target.selectionStart = e.target.selectionEnd = cursorPosition+1; }, 0);
+    }
+  };
   return (
     <div className="feedback">
     
 
       {/* Larger Grey Container with Side-by-Side Text Areas */}
       <div className="large-grey-container">
-        <textarea className="big-text-box" placeholder="Code"></textarea>
-        <textarea className="big-text-box" placeholder="Explanations"></textarea>
-        <textarea className="big-text-box" placeholder="Screen"></textarea>
+        <textarea className="big-text-box" placeholder="Code" onKeyDown={handleKeyDown}></textarea>
+        <textarea className="big-text-box" placeholder="Explanations" onKeyDown={handleKeyDown}></textarea>
+        <textarea className="big-text-box" placeholder="Screen" onKeyDown={handleKeyDown}></textarea>
       </div>
     </div>
   );

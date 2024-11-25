276f67761891c04509e10a4bac630655d50f04ef
antAmaine <157142587+antAmaine@users.noreply.github.com>
2024-11-24 20:16:22 -0500

    Cleaned up Feedback.js
    removed unused  handlekeydown function from feedback.js

--- nocom/src/components/Feedback/Feedback.js
+++ nocom/src/components/Feedback/Feedback.js
@@ -3,20 +3,6 @@ import './Feedback.css';
 import InputBox from '../InputBox';
 
 function Feedback() {
-  const handleKeyDown = (e) => {
-    if (e.key === 'Tab') {
-      e.preventDefault();
-      let value = e.target.value
-      const cursorPosition = e.target.selectionStart;
-      let i = cursorPosition
-      while(value[i-1] != '\n' && i-1>=0){
-        i--
-      }
-      const newValue = `${value.substring(0, i)}\t${value.substring(i)}`;
-      e.target.value= newValue
-      setTimeout(function(){ e.target.selectionStart = e.target.selectionEnd = cursorPosition+1; }, 0);
-    }
-  };
   return (
     <div className="feedback">
     

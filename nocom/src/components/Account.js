9efa27887c7c1cc4c43022e6df24ed8e69528069
Obed Appiah <98442128+obedappiah@users.noreply.github.com>
2024-11-26 13:46:29 -0500

    added account button

--- nocom/src/components/Account.js
+++ nocom/src/components/Account.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import './Account.css';
+import React, { useState } from "react";
+import "./Account.css";
 
 function Account() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
-    setIsOpen(!isOpen); // Toggle the dropdown visibility
+    setIsOpen(!isOpen);
   };
-
   return (
     <div className="account-container">
-      {/* Account Button */}
       <button className="Account" onClick={toggleDropdown}>
-        <i className="fas fa-user-circle"></i>
         Account
       </button>
-
-      {/* Dropdown Menu */}
       {isOpen && (
         <div className="account-dropdown">
           <ul>
-            <li><a href="/profile">Profile</a></li>
-            <li><a href="/settings">Settings</a></li>
-            <li><a href="/logout">Logout</a></li>
+            <li>
+              <a href="/profile">Profile</a>
+            </li>
+            <li>
+              <a href="/settings">Settings</a>
+            </li>
+            <li>
+              <a href="/logout">Logout</a>
+            </li>
           </ul>
         </div>
       )}
     </div>
   );
 }
-
 export default Account;

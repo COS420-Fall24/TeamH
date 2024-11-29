9affc6a39df0099fc50c8c42186bd6a86541b734
KemptonM <kempton.maillett@maine.edu>
2024-11-28 19:13:14 -0500

    include OllamaChat.js in App.js

--- nocom/src/App.js
+++ nocom/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import InputBox from './InputBox';
 import SvgMockup from './SvgMockup'; // Ensure this path matches the location of SvgMockup.js
 import LoginWidget from './LoginWidget'; // Your existing login component
+import OllamaChat from './OllamaChat'; // Import the new component
 
 const App = () => {
   return (
     <div>
       <SvgMockup /> {/* Displays the Screen.svg */}
       <LoginWidget /> {/* Your login form or other content */}
+      <OllamaChat /> {/* Add the new component here */}
     </div>
   );
 };

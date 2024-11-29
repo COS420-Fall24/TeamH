000b258dc7e528399f6c41132dca165b68b20553
KemptonM <kempton.maillett@maine.edu>
2024-11-29 01:48:45 -0500

    refactor: update ReactDOM rendering to use createRoot for improved performance

--- nocom/src/index.js
+++ nocom/src/index.js
@@ -1,12 +1,13 @@
 /* istanbul ignore file */
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css"; // Optional if you have CSS
-import App from "./App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );

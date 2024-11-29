ecadc85e0256ee0ac311a7fc0a5dd33e2133d74c
KemptonM <kempton.maillett@maine.edu>
2024-11-28 22:37:25 -0500

    update feedback component to read response from state

--- nocom/src/components/Feedback/Feedback.js
+++ nocom/src/components/Feedback/Feedback.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./Feedback.css";
 import InputBox from "../InputBox";
 
 function Feedback() {
+  const location = useLocation();
+  const { response } = location.state || {};
+
   return (
     <div className="feedback">
       {/* Larger Grey Container with Side-by-Side Text Areas */}
@@ -11,6 +15,12 @@ function Feedback() {
         <InputBox className="big-text-box" placeholder="Explanation" />
         <InputBox className="big-text-box" placeholder="Screen" />
       </div>
+      {response && (
+        <div className="output-container">
+          <h2>Explanation</h2>
+          <p>{response}</p>
+        </div>
+      )}
     </div>
   );
 }

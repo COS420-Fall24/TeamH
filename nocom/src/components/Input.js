7d97348807ae084af3830906dad8e65fcef6c3dc
KemptonM <kempton.maillett@maine.edu>
2024-12-04 20:07:13 -0500

    fix: correct variable casing in Ollama API call messages in Input.js

--- nocom/src/components/Input.js
+++ nocom/src/components/Input.js
@@ -23,7 +23,7 @@ function Input() {
     // Call the Ollama API
     const res = await ollama.chat({
       model: 'llama3.2', // Ensure this model name is correct or update it
-      messages: [{ role: 'user', content: `You are an application designed for Helping beginner programmers learn to read code.:\n\nThe context provided by the user is: ${context}\n\The code is:\n${code}` }],
+      messages: [{ role: 'user', content: `You are an application designed for Helping beginner programmers learn to read code.:\n\nThe context provided by the user is: ${Context}\n\The code is:\n${Code}` }],
     });
     setResponse(res.message.content);
 
